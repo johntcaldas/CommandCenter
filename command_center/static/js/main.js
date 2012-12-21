45f22275a67a993ad1fcc831981448666dc92957
John Caldas <jcaldas@apperian.com>
2012-12-21 17:30:58 -0500

    First pass at js/css.

--- command_center/static/js/main.js
+++ command_center/static/js/main.js
@@ -1,14 +1,37 @@
 function Main() {
 
     var self = this;
+    var m_nav_list_items = null;
     var m_footer = null;
     var m_date = '';
 
     this.initialize = function () {
-
+        m_nav_list_items = $('#topnav li');
         m_footer = $('#footer');
-
+        self.setupTabbedNavigation();
         self.getDate();
+    };
+
+    this.setupTabbedNavigation = function() {
+        // First set click handlers on the list elements.
+        m_nav_list_items.each(function(index) {
+           $(this).click(function(){
+               self.tabClick();
+           });
+        });
+
+        // Then set the first to selected.
+        m_nav_list_items.find('li:first').addClass('navTabSelected');
+    };
+
+    this.tabClick = function() {
+        // First set all tabs to not selected
+        m_nav_list_items.each(function(index) {
+           $(this).removeClass('navTabSelected');
+        });
+
+        // Then select 'this' tab
+        $(this).addClass('navTabSelected');
     };
 
     this.getDate = function() {
